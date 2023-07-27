8e39038fce6aa54711925210739fe1cc741784b5
Koushil Mankali <koushilm@italentdigital.com>
2023-07-27 14:23:34 +0530

    removed env

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Header />
+      <h1>Home</h1>
+      <Footer />
+    </>
+  );
 }
